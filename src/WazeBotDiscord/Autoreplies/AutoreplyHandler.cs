f3a111560fdc52e8050e49ac6ec2a151b9e912a9
justins83 <justins83.waze@gmail.com>
2017-09-25 10:10:39 -0400

    Test code
    Some test code for syncing discord channels...shouldn't be used, just
    want to keep for future implementation

--- src/WazeBotDiscord/Autoreplies/AutoreplyHandler.cs
+++ src/WazeBotDiscord/Autoreplies/AutoreplyHandler.cs
@@ -1,11 +1,12 @@
 ﻿using Discord.WebSocket;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace WazeBotDiscord.Autoreplies
 {
     public static class AutoreplyHandler
     {
-        public static async Task HandleAutoreplyAsync(SocketMessage inMsg, AutoreplyService arService)
+        public static async Task HandleAutoreplyAsync(SocketMessage inMsg, AutoreplyService arService)//, IReadOnlyCollection<SocketGuild> guilds)
         {
             if (inMsg.Channel is SocketDMChannel)
                 return;
@@ -14,6 +15,20 @@ namespace WazeBotDiscord.Autoreplies
             var content = msg.Content.ToLowerInvariant();
             var channel = (SocketTextChannel)msg.Channel;
 
+            //you can get the channel from the guild with GetTextChannelAsync then SendMessageAsync on it like any other channel
+            /*if (msg.Channel.Id == 359327158944137228) {
+                SocketGuild syncGuild = null;
+                foreach(SocketGuild g  in guilds)
+                {
+                    if (g.Id == 360595895965843456)
+                        syncGuild = g;
+                }
+
+                
+                var syncChannel = syncGuild.GetTextChannel(361352875680595969);
+                await syncChannel.SendMessageAsync($"**{inMsg.Author.Username}**: {inMsg.Content}");
+            }*/
+            
             var ar = arService.SearchForAutoreply(content, channel);
             if (ar == null)
                 return;
