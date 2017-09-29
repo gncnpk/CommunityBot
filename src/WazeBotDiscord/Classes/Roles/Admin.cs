a26501296ab8d1af5295736b5a955757055b31f4
justins83 <justins83.waze@gmail.com>
2017-09-29 17:33:23 -0400

    Updating ids

--- src/WazeBotDiscord/Classes/Roles/Admin.cs
+++ src/WazeBotDiscord/Classes/Roles/Admin.cs
@@ -7,11 +7,11 @@ namespace WazeBotDiscord.Classes.Roles
         public static IReadOnlyDictionary<ulong, ulong> Ids = new Dictionary<ulong, ulong>
         {
             [300471946494214146] = 363427151044673551, // National
-            //[313435914540154890] = , // Northwest
-            //[301113669696356352] = , // Southwest
-            //[313433524130545664] = , // Plains
+            [313435914540154890] = 313435983636856833, // Northwest
+            [301113669696356352] = 363437599181897729, // Southwest
+            [313433524130545664] = 313433556468760586, // Plains
             [313431377724964876] = 363428691881099275, // South Central
-            //[299563059695976451] = , // Great Lakes
+            [299563059695976451] = 363432950982770688, // Great Lakes
             [300737538384199681] = 363428105571794947, // South Atlantic
             //[313428729739083776] = , // Southeast
             //[300482201198395417] = , // New England
