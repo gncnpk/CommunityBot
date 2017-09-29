aa538a9e23ab5eb81e56fffcda7050c2f0a925af
justins83 <justins83.waze@gmail.com>
2017-09-29 17:01:27 -0400

    Setting admin ids

--- src/WazeBotDiscord/Classes/Roles/Admin.cs
+++ src/WazeBotDiscord/Classes/Roles/Admin.cs
@@ -6,17 +6,17 @@ namespace WazeBotDiscord.Classes.Roles
     {
         public static IReadOnlyDictionary<ulong, ulong> Ids = new Dictionary<ulong, ulong>
         {
-            //[300471946494214146] = , // National
+            [300471946494214146] = 363427151044673551, // National
             //[313435914540154890] = , // Northwest
             //[301113669696356352] = , // Southwest
             //[313433524130545664] = , // Plains
-            //[313431377724964876] = , // South Central
+            [313431377724964876] = 363428691881099275, // South Central
             //[299563059695976451] = , // Great Lakes
-            //[300737538384199681] = , // South Atlantic
+            [300737538384199681] = 363428105571794947, // South Atlantic
             //[313428729739083776] = , // Southeast
             //[300482201198395417] = , // New England
             //[300481818619150336] = , // Northeast
-            //[299676784327393281] = , // Mid Atlantic
+            [299676784327393281] = 363428487111114753, // Mid Atlantic
             [347386780074377217] = 361110624958152704 // Map Raid
             //[360595895965843456] = , // Community Test Server
             //[356076662812573698] =   // VEOC
