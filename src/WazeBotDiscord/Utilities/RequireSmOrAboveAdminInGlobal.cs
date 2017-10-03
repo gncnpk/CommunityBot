0b190edc00f034369825a8d98af7b5ffde8180f0
justins83 <justins83.waze@gmail.com>
2017-10-03 12:43:56 -0400

    Fixing admin role id lookup
    Fixing the admin role id lookup - was hardcoded, changing to look up
    from the Admin class.

--- src/WazeBotDiscord/Utilities/RequireSmOrAboveAdminInGlobal.cs
+++ src/WazeBotDiscord/Utilities/RequireSmOrAboveAdminInGlobal.cs
@@ -24,7 +24,7 @@ namespace WazeBotDiscord.Utilities
             var cmRole = guild.GetRole(roleId);
 
             if ((context.Guild.Id != 347386780074377217 && ((SocketGuildUser)context.Message.Author).Hierarchy >= cmRole.Position) //if on any server but Global & role >= SM
-                || (context.Guild.Id == 347386780074377217 && ((SocketGuildUser)context.Message.Author).Roles.Any(r => (r.Id == 361110624958152704)))) //or if on Global and role == admin
+                || (context.Guild.Id == 347386780074377217 && ((SocketGuildUser)context.Message.Author).Roles.Any(r => (r.Id == Admin.Ids[347386780074377217])))) //or if on Global and role == admin
                 return PreconditionResult.FromSuccess();
 
             if(context.Guild.Id == 347386780074377217) //if on Global, must be admin
