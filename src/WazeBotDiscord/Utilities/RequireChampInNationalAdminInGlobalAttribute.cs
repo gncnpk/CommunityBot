9a991cc37cd8d3d46a97ea472d3f45827db68355
justins83 <justins83.waze@gmail.com>
2017-10-03 12:44:36 -0400

    Changing admin id lookup
    Chaning the admin id lookup to use the Admin class rather than
    hardcoding.

--- src/WazeBotDiscord/Utilities/RequireChampInNationalAdminInGlobalAttribute.cs
+++ src/WazeBotDiscord/Utilities/RequireChampInNationalAdminInGlobalAttribute.cs
@@ -3,6 +3,7 @@ using Discord.WebSocket;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using WazeBotDiscord.Classes.Roles;
 
 namespace WazeBotDiscord.Utilities
 {
@@ -19,7 +20,7 @@ namespace WazeBotDiscord.Utilities
                 return PreconditionResult.FromError("That command can only be used on the national & global server.");
 
             //National server and global or local champ roles OR Global server and admin role
-            if (((SocketGuildUser)context.Message.Author).Roles.Any(r => (context.Guild.Id == 300471946494214146 && (r.Id == 300494132839841792 || r.Id == 300494182403801088)) || (context.Guild.Id == 347386780074377217 && (r.Id == 361110624958152704))))
+            if (((SocketGuildUser)context.Message.Author).Roles.Any(r => (context.Guild.Id == 300471946494214146 && (r.Id == 300494132839841792 || r.Id == 300494182403801088)) || (context.Guild.Id == 347386780074377217 && (r.Id == Admin.Ids[347386780074377217]))))
                 return PreconditionResult.FromSuccess();
 
             if(context.Guild.Id == 300471946494214146)
