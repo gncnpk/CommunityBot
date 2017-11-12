c2128b0d9eee95cbbc4ef034b5adc234e61a462a
justins83 <justins83.waze@gmail.com>
2017-11-11 22:49:59 -0500

    Adding !assign command
    Adding the !assign command, mostly to be used on the Global server for assigning mapraid roles

---
+++ src/WazeBotDiscord/Modules/AssignModule.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WazeBotDiscord.Utilities;
+
+namespace WazeBotDiscord.Modules
+{
+    public class AssignModule : ModuleBase
+    {
+        [Command("assign")]
+        [RequireChampInNationalGuild]
+        public async Task AssignRoles(IRole role, params IUser[] users)
+        {
+            foreach (SocketGuildUser myuser in users)
+            {
+                if (!myuser.Roles.Contains(role))
+                    await myuser.AddRoleAsync(role);
+            }
+
+            await ReplyAsync("Assignment complete.");
+        }
+    }
+}
