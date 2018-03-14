e2db7de81635dd4c37e5ecc22be8b419276554c5
justins83 <justins83.waze@gmail.com>
2018-03-14 11:44:47 -0400

    Adding missing roles...
    Forgot the wme and android roles, and tying to admin/moderator for global

--- src/WazeBotDiscord/Modules/RoleNoSyncModule.cs
+++ src/WazeBotDiscord/Modules/RoleNoSyncModule.cs
@@ -10,9 +10,9 @@ namespace WazeBotDiscord.Modules
     public class RoleNoSyncModule : ModuleBase
     {
         [Command("iosbeta", RunMode = RunMode.Async)]
-        [RequireCmOrAbove]
+        [RequireAdminModeratorInGlobal]
         [RequireBotPermission(GuildPermission.ManageRoles)]
-        public async Task ToggleCm(IUser user)
+        public async Task ToggleiOSBeta(IUser user)
         {
             if (IsSelf(user))
             {
@@ -31,6 +31,56 @@ namespace WazeBotDiscord.Modules
             else if (result == SyncedRoleStatus.Removed)
             {
                 await msg.ModifyAsync(m => m.Content = $"{user.Mention}: Removed iOS beta role.");
+            }
+        }
+
+        [Command("wmebeta", RunMode = RunMode.Async)]
+        [RequireAdminModeratorInGlobal]
+        [RequireBotPermission(GuildPermission.ManageRoles)]
+        public async Task ToggleWMEBeta(IUser user)
+        {
+            if (IsSelf(user))
+            {
+                await ReplyAsync("You can't change this role for yourself.");
+                return;
+            }
+
+            var msg = await ReplyAsync($"{user.Mention}: Just a moment...");
+
+            var result = await RoleAssignmentHelper.ToggleRoleAsync(user, WMEBeta.Ids, Context);
+
+            if (result == SyncedRoleStatus.Added)
+            {
+                await msg.ModifyAsync(m => m.Content = $"{user.Mention}: Added WME beta role.");
+            }
+            else if (result == SyncedRoleStatus.Removed)
+            {
+                await msg.ModifyAsync(m => m.Content = $"{user.Mention}: Removed WME beta role.");
+            }
+        }
+
+        [Command("androidbeta", RunMode = RunMode.Async)]
+        [RequireAdminModeratorInGlobal]
+        [RequireBotPermission(GuildPermission.ManageRoles)]
+        public async Task ToggleAndroidBeta(IUser user)
+        {
+            if (IsSelf(user))
+            {
+                await ReplyAsync("You can't change this role for yourself.");
+                return;
+            }
+
+            var msg = await ReplyAsync($"{user.Mention}: Just a moment...");
+
+            var result = await RoleAssignmentHelper.ToggleRoleAsync(user, AndroidBeta.Ids, Context);
+
+            if (result == SyncedRoleStatus.Added)
+            {
+                await msg.ModifyAsync(m => m.Content = $"{user.Mention}: Added Android beta role.");
+            }
+            else if (result == SyncedRoleStatus.Removed)
+            {
+                await msg.ModifyAsync(m => m.Content = $"{user.Mention}: Removed Android beta role.");
             }
         }
 
