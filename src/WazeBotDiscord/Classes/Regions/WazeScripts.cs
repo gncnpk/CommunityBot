9f39074cf8fa04f21641ab47246af6d24906e136
justins83 <justins83.waze@gmail.com>
2018-06-12 13:35:28 -0400

    Adding WazeScripts region

---
+++ src/WazeBotDiscord/Classes/Regions/WazeScripts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WazeBotDiscord.Classes.Regions
+{
+    class WazeScripts
+    {
+        public static class WazeScriptsRoles
+        {
+            public const ulong StateManager = 456101008280518658;
+        }
+    }
+}
