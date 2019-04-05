decab37f4c48138c32abaf1c4989e79adb8ca47d
justins83 <justins83.waze@gmail.com>
2019-04-05 17:37:49 -0400

    Bug fix

--- src/WazeBotDiscord/ChannelSync/ChannelSyncHandler.cs
+++ src/WazeBotDiscord/ChannelSync/ChannelSyncHandler.cs
@@ -17,26 +17,24 @@ namespace WazeBotDiscord.ChannelSync
                 return;
 
             var channels = service.getSyncChannels(msg.Channel.Id);
-            ulong channelToSyncTo = channels.Channel1;
+            if (channels != null) {
+                ulong channelToSyncTo = channels.Channel1;
 
-            if (msg.Channel.Id == channels.Channel1)
-                channelToSyncTo = channels.Channel2;
+                if (msg.Channel.Id == channels.Channel1)
+                    channelToSyncTo = channels.Channel2;
 
-            string nickname = msg.Author.Username;
-            if (msg.Author is Discord.WebSocket.SocketGuildUser)
-                nickname = ((Discord.WebSocket.SocketGuildUser)msg.Author).Nickname;
+                string nickname = msg.Author.Username;
+                if (msg.Author is Discord.WebSocket.SocketGuildUser)
+                    if (((Discord.WebSocket.SocketGuildUser)msg.Author).Nickname != null)
+                        nickname = ((Discord.WebSocket.SocketGuildUser)msg.Author).Nickname;
 
-            var author = new EmbedAuthorBuilder().WithName(nickname).WithIconUrl(msg.Author.GetAvatarUrl());
+                var author = new EmbedAuthorBuilder().WithName(nickname).WithIconUrl(msg.Author.GetAvatarUrl());
 
-            var embed = new EmbedBuilder().WithAuthor(author).WithDescription(msg.Content);
+                var embed = new EmbedBuilder().WithAuthor(author).WithDescription(msg.Content);
 
-            var myChannel = client.GetChannel(channelToSyncTo) as IMessageChannel;
-            await myChannel.SendMessageAsync("",false, embed);
-
-            //DiscordSocketClient _client = new DiscordSocketClient(); // 2
-            //ulong id = 123456789012345678; // 3
-            //var chnl = _client.GetChannel(id) as IMessageChannel; // 4
-            //await chnl.SendMessageAsync("Announcement!"); // 5
+                var myChannel = client.GetChannel(channelToSyncTo) as IMessageChannel;
+                await myChannel.SendMessageAsync("", false, embed);
+            }
 
         }
     }
