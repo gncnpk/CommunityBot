8a6192b9bdd6cd5de332ebb504782833a2f0cfee
justins83 <justins83.waze@gmail.com>
2019-07-26 11:12:55 -0400

    Update TilesModule.cs
    Fixing the tiles command - Waze changed the international designation from INTL to "International" in the status data

--- src/WazeBotDiscord/Modules/TilesModule.cs
+++ src/WazeBotDiscord/Modules/TilesModule.cs
@@ -26,7 +26,7 @@ namespace WazeBotDiscord.Modules
             string body = await httpClient.GetStringAsync("https://status.waze.com/feeds/posts/default");//("https://wazestatus.wordpress.com/category/main/feed");
             Regex regNA = new Regex(@"<published>(.{29})<\/published><updated>.{29}<\/updated><title type='text'>NA map tiles were successfully updated to: (.*?)<\/title>");//new Regex(" < title>NA map tiles were successfully updated to:(.*?)<\\/title>(.|\\n)*?<pubDate>(.*?) \\+0000<\\/pubDate>");
             Match matchNA = regNA.Match(body);
-            Regex regINTL = new Regex(@"<published>(.{29})<\/published><updated>.{29}<\/updated><title type='text'>INTL map tiles were successfully updated to: (.*?)<\/title>");//new Regex(" < title>INTL map tiles were successfully updated to:(.*?)<\\/title>(.|\\n)*?<pubDate>(.*?) \\+0000<\\/pubDate>");
+            Regex regINTL = new Regex(@"<published>(.{29})<\/published><updated>.{29}<\/updated><title type='text'>International map tiles were successfully updated to: (.*?)<\/title>");//new Regex(" < title>INTL map tiles were successfully updated to:(.*?)<\\/title>(.|\\n)*?<pubDate>(.*?) \\+0000<\\/pubDate>");
             Match matchINTL = regINTL.Match(body);
             DateTime NAPubDateTime = DateTime.Parse(matchNA.Groups[1].ToString());
             DateTime INTLPubDateTime = DateTime.Parse(matchINTL.Groups[1].ToString());
