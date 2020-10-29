83da636e3b1b227fa10dde3f661b8e40e6dd75a7
Justin <justins83.waze@gmail.com>
2020-10-29 15:24:29 -0400

    Update RequireChampInNationalAdminInGlobalAttribute.cs
    Upadting to use Servers enum

--- src/WazeBotDiscord/Utilities/RequireChampInNationalAdminInGlobalAttribute.cs
+++ src/WazeBotDiscord/Utilities/RequireChampInNationalAdminInGlobalAttribute.cs
@@ -21,7 +21,7 @@ namespace WazeBotDiscord.Utilities
                 return PreconditionResult.FromError("That command can only be used on the national & global server.");
 
             //National server and global or local champ roles OR Global server and admin role
-            if (((SocketGuildUser)context.Message.Author).Roles.Any(r => (context.Guild.Id == 300471946494214146 && (r.Id == 300494132839841792 || r.Id == 300494182403801088)) || (context.Guild.Id == 347386780074377217 && (r.Id == Admin.Ids[347386780074377217]))))
+            if (((SocketGuildUser)context.Message.Author).Roles.Any(r => (context.Guild.Id == 300471946494214146 && (r.Id == 300494132839841792 || r.Id == 300494182403801088)) || (context.Guild.Id == Servers.GlobalMapraid && (r.Id == Admin.Ids[Servers.GlobalMapraid]))))
                 return PreconditionResult.FromSuccess();
 
             if(context.Guild.Id == 300471946494214146)
